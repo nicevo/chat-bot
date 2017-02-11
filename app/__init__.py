8203dd821438db2fc2d15be7a09bcc93cdc995fd
Mikael Svensson <mikael.svensson@klarna.com>
2017-02-11 21:38:55 +0100

    Challenge 4: Telegram bot (library dependency and simple callback endpoint)

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
+import os
+
 from flask import Flask
+from twx.botapi import TelegramBot
 
 application = Flask(__name__)
+
+bot_api_key = os.environ.get('TELEGRAM_BOT_APIKEY')
+
+bot = TelegramBot(bot_api_key)
 
 from app import views
--- run.py
+++ run.py
@@ -1,4 +1,35 @@
 #!/usr/bin/python
-from app import application
+import os
 
-application.run(debug=True)
+from twx.botapi import InputFileInfo, InputFile
+
+from app import application
+from app import bot
+
+
+def get_certificate():
+    certificate_path = 'app/ssl-certificate.herokuapp-com.pem'
+    fp = open(certificate_path, 'rb')
+    file_info = InputFileInfo(
+        file_name=certificate_path,
+        fp=fp,
+        mime_type='text/plain')
+    return InputFile('certificate', file_info)
+
+
+def init_bot(telegram_bot):
+    telegram_bot.update_bot_info().wait()
+    print 'Starting bot %s' % telegram_bot.username
+    certificate = get_certificate()
+    telegram_bot.set_webhook(
+        url='https://helloworld-telegrambot.herokuapp.com/incoming',
+        certificate=certificate,
+    ).wait()
+    print 'Webhook configured'
+
+
+try:
+    init_bot(bot)
+    application.run(debug=True, threaded=True)
+except Exception as e:
+    print e
