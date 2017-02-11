f2f6eab58d7e2728ed32404ae060e455821ed6b4
Mikael Svensson <mikael.svensson@klarna.com>
2017-02-11 21:28:04 +0100

    Challenge 4: Kik bot (library dependency and simple callback endpoint)

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,16 @@
+import os
+
 from flask import Flask
+from kik import KikApi, Configuration
 
 application = Flask(__name__)
+
+bot_username = os.environ.get('KIK_BOT_USERNAME')
+bot_api_key = os.environ.get('KIK_BOT_APIKEY')
+
+kik = KikApi(bot_username, bot_api_key)
+
+kik.set_configuration(Configuration(
+    webhook='https://A_NAME_OF_YOUR_CHOICE.herokuapp.com/callback/kik'))
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 import time
-from flask import render_template
+from flask import render_template, request, Response
+from kik.messages import messages_from_json, TextMessage
+
 from app import application
+from app import kik
 
 
 @application.route('/')
@@ -14,3 +17,23 @@ def show_time():
         'time.html',
         now=time.strftime("%c")
     )
+
+
+@application.route('/callback/kik', methods=['POST'])
+def incoming():
+    if not kik.verify_signature(request.headers.get('X-Kik-Signature'), request.get_data()):
+        return Response(status=403)
+
+    messages = messages_from_json(request.json['messages'])
+
+    for message in messages:
+        if isinstance(message, TextMessage):
+            kik.send_messages([
+                TextMessage(
+                    to=message.from_user,
+                    chat_id=message.chat_id,
+                    body=message.body
+                )
+            ])
+
+    return Response(status=200)
