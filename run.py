333eeb74ec87ef33bf44ac61b68e5a103dfee4f9
Mikael Svensson <mikael.svensson@klarna.com>
2017-02-15 20:15:07 +0100

    Challenge 4: Deliberately configured the wrong webhook URL

--- run.py
+++ run.py
@@ -22,7 +22,7 @@ def init_bot(telegram_bot):
     print 'Starting bot %s' % telegram_bot.username
     certificate = get_certificate()
     telegram_bot.set_webhook(
-        url='https://helloworld-telegrambot.herokuapp.com/incoming',
+        url='https://A_NAME_OF_YOUR_CHOICE.herokuapp.com/incoming',
         certificate=certificate,
     ).wait()
     print 'Webhook configured'
