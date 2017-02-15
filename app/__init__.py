66404e3067eb5d705d174be445bd524035845df4
Mikael Svensson <mikael.svensson@klarna.com>
2017-02-15 20:04:10 +0100

    Challenge 2: Heroku logging

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
+import os
 from flask import Flask
 
 application = Flask(__name__)
 
 from app import views
+
+if os.environ.get('HEROKU') is not None:
+    import logging
+
+    stream_handler = logging.StreamHandler()
+    application.logger.addHandler(stream_handler)
+    application.logger.setLevel(logging.DEBUG)
