6946b0fd179e1902e3ea267e7dcc6e31110dfb86
Mariana <mariana.bocoi@gmail.com>
2017-06-30 08:02:05 +0200

    Fixed print for python 3

--- app/views.py
+++ app/views.py
@@ -5,6 +5,12 @@ from app import application
 from app import bot
 import json
 
+def process_text(message):
+    incomming_text = message.text
+    # Your code goes here
+    my_response = incomming_text
+    return my_response
+
 
 @application.route('/')
 def index():
@@ -25,14 +31,14 @@ def incoming():
     m = j['message']
     msg = Message.from_result(m)
     print('Raw message:', msg)
-
     try:
         print('Received this message from user %d (%s): %s' % (msg.sender.id, msg.sender.first_name, msg.text))
         chat_id = msg.chat.id
         print('Responding to chat %i using token %s' % (chat_id, bot.token))
+        response_text = process_text(msg)
         resp = bot.send_message(
             chat_id=chat_id,
-            text=msg.text,
+            text=response_text,
             parse_mode=None,
             disable_web_page_preview=None,
             reply_to_message_id=None,
@@ -44,3 +50,6 @@ def incoming():
     print("send_message returned ", resp)
 
     return Response(status=200)
+
+
+
